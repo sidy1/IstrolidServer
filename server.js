4a02a449be6245c9c4cd0142d3ab85d0abb53f1a
sidy1 <sidy1@theboxnet.com>
2020-01-04 18:20:52 +0000

    changed Ports

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ global.Server = function() {
         // Send server info
         let info = {
             name: config.name,
-            address: "ws://" + config.addr + ":" + PORTN,
+            address: "ws://" + "0.0.0.0" + ":" + process.env.PORT || 3000;,
             observers: sim.players.filter(p => p.connected).length,
             players: sim.players.filter(p => p.connected && !p.ai).map(p => { return {
                 name: p.name,
